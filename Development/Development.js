d22a23255116cf0d4666a281db775427a0eeeb15
Elite Robust Ontology <erandix9@gmail.com>
2026-01-01 17:51:26 +0300

    Updates

--- Development/Development.js
+++ Development/Development.js
@@ -6,6 +6,74 @@ const ERANDES=()=>{
 
 const MOBILEVIEW=()=>{
 
+    HEADERVIEW((ELEMENT)=>{
+
+        TEXT(ELEMENT,"h1","Image Convertor","",()=>{
+
+        });
+
+    },(ELEMENT)=>{
+
+        BREAK(ELEMENT);
+
+        IMAGE(ELEMENT,"95%","50%",ERANDIXLOGO,TRANSPARENT,(ELEMENTERS)=>{
+
+            INLINEVIEW(ELEMENT,"95%","100px","","5% 0%",(ELEMENTS)=>{
+
+                TEXTAREA(ELEMENT,"",(ELEMS)=>{
+
+                    HEIGHT(ELEMS,"30%");
+
+                    BUTTON(ELEMENTS,"Change",TEAL,"",(ELEMSA)=>{
+
+                        WIDTH(ELEMSA,"20%");
+
+                        FILEPICKER(ELEMSA,(Data)=>{
+
+                            SOURCED(ELEMENTERS,Data.data);
+
+                            BASE64CONVERTOR(Data.data, (Datata)=>{
+
+                                BUTTON(ELEMENTS,"Copy",ORANGE,"",(ELEMSA)=>{
+
+                                    WIDTH(ELEMSA,"20%");
+
+                                    CLICK(ELEMSA,()=>{
+
+                                        COPY(Datata);
+
+                                    });
+
+                                });
+
+                                TEXTCHANGER(ELEMS,Datata);
+
+                            });
+
+                        });
+
+                    });
+
+                    BUTTON(ELEMENTS,"Delete",RED,"",(ELEMSA)=>{
+
+                        WIDTH(ELEMSA,"20%");
+
+                        CLICK(ELEMSA,()=>{
+
+                            RELOAD();
+
+                        });
+
+                    });
+
+                });
+
+            });
+
+        });
+
+    });
+
 };
 
 const DESKTOPVIEW=()=>{
