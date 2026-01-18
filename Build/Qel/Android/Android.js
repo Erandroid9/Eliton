552540c275d9a23a277420ad9842756e2610431f
Elite Robust Ontology <erandix9@gmail.com>
2026-01-18 06:58:34 +0300

    Updates

--- Build/Qel/Android/Android.js
+++ Build/Qel/Android/Android.js
@@ -12,15 +12,21 @@ const HOMEPAGE=()=>{
 
             REDUX(data,(Data)=>{
 
-                DIVVIEW(ELEMENT,"95%","300px",ORANGE,(ELEMENTS0)=>{
+                DIVVIEW(ELEMENT,"95%","300px",TRANSPARENT,(ELEMENTS0)=>{
 
                     MARGIN(ELEMENTS0,"2%");
-                    
+
                     OVERFLOW(ELEMENTS0);
 
-                });
+                    HEADER(ELEMENTS0,(ELES)=>{
 
-                console.log(Data);
+                        LEFTTEXTVIEW(ELES,Data.ProductName,(ELEMENTSA)=>{
+
+                        });
+
+                    });
+
+                });
 
             });
 
@@ -30,15 +36,134 @@ const HOMEPAGE=()=>{
 
         ICONVIEW(ELEMENT,WHITEGRIDICON,(ELEMENTS)=>{
 
+            CLICK(ELEMENTS,()=>{
+
+                ROUTE(" ",CARTERGORYPAGE,"HOMEPAGE");
+
+            }); 
+
         });
 
         ICONVIEW(ELEMENT,WHITESHOPPINGCART,(ELEMENTS)=>{
+
+            CLICK(ELEMENTS,()=>{
+
+                ROUTE(" ",SHOPPINGACCOUNTPAGE,"HOMEPAGE");
+
+            });
 
         });
 
         ICONVIEW(ELEMENT,WHITEUSERPROFILEICON,(ELEMENTS)=>{
 
+            CLICK(ELEMENTS,()=>{
+
+                ROUTE(" ",USERACCOUNTPAGE,"HOMEPAGE");
+
+            });
+
         });
+
+    });
+
+};
+
+const USERACCOUNTPAGE=()=>{
+
+    HEADERVIEW((ELEMENT)=>{
+
+        LEFTICONVIEW(ELEMENT,WHITEBACKICON,(ELEMENTS)=>{
+
+            CLICK(ELEMENTS,()=>{
+
+                ROUTE("",HOMEPAGE,"HOMEPAGE");
+
+            });
+
+        });
+
+        RIGHTTEXTVIEW(ELEMENT,"Profile",(ELEMENTS)=>{
+
+        });
+
+    },(ELEMENT)=>{
+
+        DIVVIEW(ELEMENT,"95%","300px",ORANGE,(ELEMENTS)=>{
+
+            MARGIN(ELEMENTS,"2%");
+            RADIUS(ELEMENTS,"10px");
+            OVERFLOW(ELEMENTS);
+
+        });
+
+        BUTTON(ELEMENT,"Settings",FORESTGREEN,"",(ELEMENTS)=>{
+
+            WIDTH(ELEMENTS,"95%");
+            MARGIN(ELEMENTS,"2%");
+
+        });
+
+        BUTTON(ELEMENT,"Policies",FORESTGREEN,"",(ELEMENTS)=>{
+
+            WIDTH(ELEMENTS,"95%");
+            MARGIN(ELEMENTS,"2%");
+
+        });
+
+        BUTTON(ELEMENT,"Updates",FORESTGREEN,"",(ELEMENTS)=>{
+
+            WIDTH(ELEMENTS,"95%");
+            MARGIN(ELEMENTS,"2%");
+
+        });
+
+    });
+
+};
+
+const SHOPPINGACCOUNTPAGE=()=>{
+
+    HEADERVIEW((ELEMENT)=>{
+
+        LEFTICONVIEW(ELEMENT,WHITEBACKICON,(ELEMENTS)=>{
+
+            CLICK(ELEMENTS,()=>{
+
+                ROUTE("",HOMEPAGE,"HOMEPAGE");
+
+            });
+
+        });
+
+        RIGHTTEXTVIEW(ELEMENT,"My Cart",(ELEMENTS)=>{
+
+        });
+
+    },(ELEMENT)=>{
+
+    });
+
+};
+
+const CARTERGORYPAGE=()=>{
+
+    HEADERVIEW((ELEMENT)=>{
+
+        LEFTICONVIEW(ELEMENT,WHITEBACKICON,(ELEMENTS)=>{
+
+            CLICK(ELEMENTS,()=>{
+
+                ROUTE("",HOMEPAGE,"HOMEPAGE");
+
+            });
+
+        });
+
+        RIGHTTEXTVIEW(ELEMENT,"Catergroy",(ELEMENTS)=>{
+
+        });
+
+    },(ELEMENT)=>{
 
     });
 
