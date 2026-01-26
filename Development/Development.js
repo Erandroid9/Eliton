7dafc887828c55e48abc5affb3da32c7585f5ec4
Elite Robust Ontology <erandix9@gmail.com>
2026-01-26 18:46:04 +0300

    Updates

--- Development/Development.js
+++ Development/Development.js
@@ -1,5 +1,7 @@
 const ERANDES=()=>{
 
+    PAGESMANAGERS();
+
     DOWNLOADSAVEINDEX(QELDATABASELINK,"Catergory",(Data)=>{
 
         DOWNLOADSAVEINDEX(QELDATABASELINK,"Products",(Data)=>{
@@ -14,7 +16,46 @@ const ERANDES=()=>{
 
         },()=>{
 
+            PAGESMANAGERS();
             
+        });
+
+    });
+
+};
+
+
+const PAGESMANAGERS=()=>{
+
+    SWITCHER("800px",()=>{DESKTOPVIEW()},()=>{MOBILEVIEW();})
+
+};
+
+const DESKTOPVIEW=()=>{
+
+};
+
+const MOBILEVIEW=()=>{
+
+    HEADERFOOTERVIEW((ELEMENT)=>{
+
+        LEFTTEXTVIEW(ELEMENT,"Qel Medistore",()=>{
+
+        });
+
+    },(ELEMENT)=>{
+
+    },(ELEMENT)=>{
+
+        TEXTVIEW(ELEMENT,"Contact Us",()=>{
+            
+        });
+
+        TEXTVIEW(ELEMENT,"App",()=>{
+            
+        });
+
+        TEXTVIEW(ELEMENT,"Policies",()=>{
             
         });
 
