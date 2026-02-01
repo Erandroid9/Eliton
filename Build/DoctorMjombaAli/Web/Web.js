f2ad746558e89ed9fecb9d2775684cf06d784299
Elite Robust Ontology <erandix9@gmail.com>
2026-02-01 12:25:05 +0300

    Updates

---
+++ Build/DoctorMjombaAli/Web/Web.js
@@ -0,0 +1,25 @@
+const ERANDES=()=>{
+
+    SWITCHER("800px",()=>{DESKTOPVIEW()},()=>{MOBILEVIEW();})
+
+};
+
+const DESKTOPVIEW=()=>{
+
+    HEADERVIEW((ELEMENT)=>{
+
+    },(ELEMENT)=>{
+
+    });
+    
+};
+
+const MOBILEVIEW=()=>{
+
+    HEADERVIEW((ELEMENT)=>{
+
+    },(ELEMENT)=>{
+
+    });
+
+};
