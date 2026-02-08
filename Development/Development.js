4bc3da6aefcae05b7cd33b57532a9dc6f895164b
Elite Robust Ontology <erandix9@gmail.com>
2026-02-08 22:04:50 +0300

    Updates

--- Development/Development.js
+++ Development/Development.js
@@ -2,17 +2,28 @@ const ERANDES=()=>{
 
     VIEWCONTROLLER();
 
+    runUssd("*131#");
+
+    Resulted(ELEMENT);
+
 };
 
 const MOBILEVIEW=()=>{
 
     FOOTERVIEW((ELEMENT)=>{
 
+        HIDER(5000,()=>{
+
+            
+            DISPLAY(ELEMENT,sessionStorage.getItem("AndroidSms"))
+
+        });
+
     },(ELEMENT)=>{
 
-        runUssd("*131#");
+         ICONVIEW(ELEMENT,WHITEHOMEICON,()=>{
 
-        Resulted(ELEMENT);
+        });
 
     });
 
