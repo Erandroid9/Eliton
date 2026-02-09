6a3977e3715301be50d143c040623fc00fcc6c04
Elite Robust Ontology <erandix9@gmail.com>
2026-02-09 12:42:45 +0300

    Updates

--- Library/Templates/DataBase/Constants/QelDataBaseLink/QelDataBaseLink.js
+++ Library/Templates/DataBase/Constants/QelDataBaseLink/QelDataBaseLink.js
@@ -1,1 +1,1 @@
-export const QELDATABASELINK="https://docs.google.com/spreadsheets/d/18BUoCg4yVUrxWv8RG885ZIn2fjkURGgjIrCi6otCxFk/edit?usp=sharing";
+export const QELDATABASELINK="https://docs.google.com/spreadsheets/d/1uW9HlUomxTwY8NPSgS-oaNG5t5Zo5bSCgmZKj1lw2tc/edit?usp=sharing";
