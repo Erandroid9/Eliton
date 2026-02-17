5291f2ccb87392e0516be85da61c215fb8eb144e
Elite Robust Ontology <erandix9@gmail.com>
2026-02-17 10:15:24 +0300

    Updates

--- Library/Functions/DataBase/OperatingSystem/OperatingSystem.js
+++ Library/Functions/DataBase/OperatingSystem/OperatingSystem.js
@@ -1,23 +1,66 @@
 export const OPERATINGSYSTEM = () => {
 
     const userAgent = navigator.userAgent || navigator.vendor || window.opera;
-    
+
     let os = "Unknown OS";
+    let deviceType = "Unknown Device";
+    let deviceName = "Unknown Device";
 
-    if (/windows phone/i.test(userAgent)) os = "Windows Phone";
+    // ===== OS Detection =====
+    if (/windows phone/i.test(userAgent)) {
+        os = "Windows Phone";
+        deviceType = "Mobile";
+    } 
+    else if (/win/i.test(userAgent)) {
+        os = "Windows";
+        deviceType = "Desktop";
+    } 
+    else if (/android/i.test(userAgent)) {
+        os = "Android";
+        deviceType = /mobile/i.test(userAgent) ? "Mobile" : "Tablet";
 
-    else if (/win/i.test(userAgent)) os = "Windows";
+        // Try detecting common Android brands
+        if (/samsung/i.test(userAgent)) deviceName = "Samsung Device";
+        else if (/huawei/i.test(userAgent)) deviceName = "Huawei Device";
+        else if (/xiaomi/i.test(userAgent)) deviceName = "Xiaomi Device";
+        else deviceName = "Android Device";
+    } 
+    else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        os = "iOS";
 
-    else if (/android/i.test(userAgent)) os = "Android";
+        if (/iPhone/.test(userAgent)) {
+            deviceType = "Mobile";
+            deviceName = "iPhone";
+        } 
+        else if (/iPad/.test(userAgent)) {
+            deviceType = "Tablet";
+            deviceName = "iPad";
+        } 
+        else {
+            deviceType = "Mobile";
+            deviceName = "iOS Device";
+        }
+    } 
+    else if (/Macintosh/i.test(userAgent)) {
+        os = "MacOS";
+        deviceType = "Desktop";
+        deviceName = "Mac";
+    } 
+    else if (/Linux/i.test(userAgent)) {
+        os = "Linux";
+        deviceType = "Desktop";
+        deviceName = "Linux Device";
+    }
 
-    else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) os = "iOS";
-   
-    else if (/Macintosh/i.test(userAgent)) os = "MacOS";
-    
-    else if (/Linux/i.test(userAgent)) os = "Linux";
-    
-    localStorage.setItem("OperatingSystem", os);
-    
-    return os;
-    
+    const deviceData = {
+        os,
+        deviceType,
+        deviceName,
+        userAgent,
+        detectedAt: new Date().toISOString()
+    };
+
+    localStorage.setItem("OperatingSystem", JSON.stringify(deviceData));
+
+    return deviceData;
 };
